8dfb4ec2e99692864b56dd04639978c32a89668e
danilo <sanfracaceresdanilo@gmail.com>
2025-03-30 20:34:14 -0300

    Mejorar el diseño del componente de carga de archivos y ajustar la posición del botón de conversión

--- src/compnets/uploads.tsx
+++ src/compnets/uploads.tsx
@@ -44,30 +44,35 @@ export function Uploads() {
 
   return (
     <>
-      <div className="flex w-full items-center justify-center">
-        <Label
-          htmlFor="dropzone-file"
-          className="flex h-64 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-600"
-        >
-          <div className="flex flex-col items-center justify-center pb-6 pt-5">
-            <Cloud />
-            <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-              <span className="font-semibold">Click to upload</span> or drag and drop
-            </p>
-            <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
-          </div>
-          <FileInput
-            onChange={handleFileChange}
-            id="dropzone-file"
-            className="hidden" />
-        </Label>
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]" />
+      <div className="flex justify-center pt-12">
+        <div className="flex w-96 items-center justify-center">
+          <Label
+            htmlFor="dropzone-file"
+            className="flex h-64 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+            <div className="flex flex-col items-center justify-center pb-6 pt-5">
+              <Cloud />
+              <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                <span className="font-semibold">Click to upload</span> or drag and drop
+              </p>
+              <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+            </div>
+            <FileInput
+              onChange={handleFileChange}
+              id="dropzone-file"
+              className="hidden" />
+          </Label>
+        </div>
       </div>
-      <Button
-        color="dark"
-        onClick={submit}
-      >
-        Convert
-      </Button>
+      <div className="flex justify-center pt-6">
+        <Button
+          className="cursor-pointer"
+          pill
+          color="light"
+          onClick={submit}>
+          Convert
+        </Button>
+      </div>
     </>
   );
 }
